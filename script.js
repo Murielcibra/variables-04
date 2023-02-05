8a56dde1a726d1d7e8854a32f5261e994ae3a7d5
Muriel Cishek <muriel_cibra@hotmail.com>
2023-02-05 17:19:52 +0100

    task-4
    variables

---
+++ script.js
@@ -0,0 +1,9 @@
+let userName = "Brad";
+userName = "Jenna";
+
+function getUserNameLength(name) {
+  return name.length;
+}
+
+console.log(getUserNameLength(userName) > 4);
+// ^______________ Should log true
