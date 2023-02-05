83dbbce6205965ad261812f8d8bd9934e229b022
Muriel Cishek <muriel_cibra@hotmail.com>
2023-02-05 17:57:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function getUserNameLength(name) {
 
 console.log(getUserNameLength(userName) > 4);
 // ^______________ Should log true
+
+// TODO: Implement the isString function
+
+function isString(data) {
+  return typeof data === "string";
+}
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined));
+// result should be false
+
+console.log(isString(""));
+// result should be true
+
+console.log(isString("John" + "Doe"));
+// result should be true
